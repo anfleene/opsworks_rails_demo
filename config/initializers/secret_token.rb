d81259a1020e41b509c4a3bdb603d7a1246b277e
Andy Fleener <andrew.fleener@sportngin.com>
2015-02-16 22:56:16 -0600

    Fully setup refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpsworksRailsDemo::Application.config.secret_token = 'c50cb387cbab47b61940419f767fb5218a013edc44b5208b793f81b838db7e613fdf2cddf4d4461b333ae77f6b83cf749d06719b47bef3d267c678686f449595'
